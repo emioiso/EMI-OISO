e4c298b669dbca249240627b3a131b024af341b3
emioiso <jijjjiijiijijiji@gmail.com>
2024-06-26 16:42:24 +0900

    HistoryViewControllerの32セクションの配置を決定できました。

---
+++ EMI OISO/HistoryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HistoryViewController.swift
+//  EMI OISO
+//
+//  Created by emi oiso on 2024/06/26.
+//
+
+import UIKit
+
+class HistoryViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
