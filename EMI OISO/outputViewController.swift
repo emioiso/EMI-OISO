2d1f07e5c20d777dd529f3ce2016f48463b4e688
emioiso <jijjjiijiijijiji@gmail.com>
2026-02-14 19:43:54 +0900

    hatenaAtsumeViewControllerにnoteのリンクを設置しました。

---
+++ EMI OISO/outputViewController.swift
@@ -0,0 +1,29 @@
+//
+//  outputViewController.swift
+//  EMI OISO
+//
+//  Created by emi oiso on 2026/02/14.
+//
+
+import UIKit
+
+class outputViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
